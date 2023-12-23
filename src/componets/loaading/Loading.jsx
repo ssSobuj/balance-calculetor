3001001759e373a9f24bd487045540a70c8fd5b2
ssSobuj <mdsobujridoy@gmail.com>
2023-12-23 09:43:08 +0600

    vercel json

--- src/componets/loaading/Loading.jsx
+++ src/componets/loaading/Loading.jsx
@@ -1,6 +1,7 @@
 
 export default function Loading() {
   return (
-    <h1 style={{fontSize:'50px', width:'100vw', height:'100vh', background:'red'}}>...............Loading</h1>
+    <h1 style={{fontSize:'200px', display:'flex', textAlign:'center',
+    alignItems:'center',justifyContent:'center', width:'100vw', height:'100vh', background:'red'}}>...............Loading</h1>
   )
 }
