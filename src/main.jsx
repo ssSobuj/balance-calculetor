6b78d7364f2eacd7b7f4a7392994096c36fc6bc5
ssSobuj <mdsobujridoy@gmail.com>
2023-12-23 09:32:09 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes/Routes.jsx";
-// import './index.css'
+import { routes } from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
